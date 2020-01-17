1faed624067d881bbb94b105d5dbc16c3efaf745
howard-stearns <howard@highfidelity.io>
2020-01-17 11:27:22 -0800

    reset, sync, and instructions

--- driver.js
+++ driver.js
@@ -6,18 +6,23 @@ class Recorder extends Croquet.View {
         recorder = this;
         this.subscribe(this.session, 'gotPing', this.gotPing);
         this.subscribe(this.session, 'gotPong', this.gotPong);
+        this.subscribe(this.viewId, "synced", this.synced);        
     }
     gotPing() {
-        console.log('gotPing');
+        console.log('gotPing', this.isSynced);
+        if (!this.isSynced) return;
         sent.innerHTML = 1 + parseInt(sent.innerHTML, 10);
         this.future(parseInt(frequency.value, 10) * 1000).maybePing();
     }
     gotPong() {
-        console.log('gotPong');        
+        console.log('gotPong', this.isSynced);        
         received.innerHTML = 1 + parseInt(received.innerHTML, 10);
     }
+    synced(isSynced) {
+        this.isSynced = isSynced;
+    }
     maybePing() {
-        if (!startButton.disabled) return;
+        if (!startButton.disabled || !this.isSynced) return;
         this.publish(this.session, 'ping');
     }
 }
--- simple.js
+++ simple.js
@@ -3,11 +3,20 @@ class Responder extends Croquet.View {
     constructor(model) {
         super(model);
         this.subscribe(this.session, 'gotPing', this.gotPing);
+        this.subscribe(this.viewId, "synced", this.synced);
+    }
+    synced(isSynced) {
+        this.isSynced = isSynced;
     }
     gotPing() {
-        console.log('gotPing');
+        console.log('gotPing', this.isSynced);
+        if (!this.isSynced) return;
         replied.innerHTML = 1 + parseInt(replied.innerHTML, 10);
         this.publish(this.session, 'pong');
     }
+}
+
+function resetClick() {
+    replied.innerHTML = 0;
 }
 Croquet.startSession(Q.APP_NAME, Messenger, Responder);
